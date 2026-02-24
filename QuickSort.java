4a31f756fadd16edc7e6ace3f250ca7c1dda13b2
Dien Tran <tuandien1998@gmail.com>
2026-02-23 17:13:06 -0800

    Heap heap hurray

--- QuickSort.java
+++ QuickSort.java
@@ -182,6 +182,56 @@ public class QuickSort
         return System.currentTimeMillis() - startTime;
     }
     // main driver program
+
+    long maxHeapify(int arr[], int n, int i) {
+        int largest = i; // Initialize largest as root
+        int l = 2 * i + 1; // left = 2*i + 1
+        int r = 2 * i + 2; // right = 2*i + 2
+
+        // If left child is larger than root
+        if (l < n && arr[l] > arr[largest])
+            largest = l;
+
+        // If right child is larger than largest so far
+        if (r < n && arr[r] > arr[largest])
+            largest = r;
+
+        // If largest is not root
+        if (largest != i) {
+            int swap = arr[i];
+            arr[i] = arr[largest];
+            arr[largest] = swap;
+
+            // Recursively heapify the affected sub-tree
+            maxHeapify(arr, n, largest);
+        }
+        return 0;
+    }
+
+    long heapSort(int arr[]) {
+        if(arr.length == 0) {
+            return 0;
+        }
+
+        int n = arr.length;
+        // Build heap (rearrange array)
+        for (int i = n / 2 - 1; i >= 0; i--)
+            maxHeapify(arr, n, i);
+
+        // One by one extract an element from heap
+        long startTime = System.currentTimeMillis();
+        for (int i = n - 1; i > 0; i--) {
+            // Move current root to end
+            int temp = arr[0];
+            arr[0] = arr[i];
+            arr[i] = temp;
+
+            // call max heapify on the reduced heap
+            maxHeapify(arr, i, 0);
+        }
+        return System.currentTimeMillis() - startTime;
+    }
+
     public static void main(String args[])
     {
         // NOTE: if you get StackOverflowError then decrease NUM_ITEMS below
@@ -229,8 +279,7 @@ public class QuickSort
         System.out.println("sorted random array, time in ms: " + Long.toString(randomTime));
         System.out.println("sorted same values array, time: " + Long.toString(sameTime));
         
-        
-        
+
         System.out.println("\nBubble Sort times with possible short circuit follow:");
         arr = getAscendingArray(NUM_ITEMS);
         ascTime = ob.bubbleSort(arr, true);
@@ -245,5 +294,28 @@ public class QuickSort
         System.out.println("sorted desc array, time in ms: " + Long.toString(descTime));
         System.out.println("sorted random array, time in ms: " + Long.toString(randomTime));
         System.out.println("sorted same values array, time: " + Long.toString(sameTime));
+
+        System.out.println("\nHeap Sort times follow:");
+        arr = getAscendingArray(NUM_ITEMS);
+        ascTime = ob.heapSort(arr);
+        arr = getDescendingArray(NUM_ITEMS);
+        descTime = ob.heapSort(arr);
+        arr = getRandomArray(NUM_ITEMS);
+        randomTime = ob.heapSort(arr);
+        //printArray(arr); // NOTE: can verify sorted correctly (for small arrays)
+        arr = getSameArray(NUM_ITEMS);
+        sameTime = ob.heapSort(arr);
+        System.out.println("sorted asc array, time in ms: " + Long.toString(ascTime));
+        System.out.println("sorted desc array, time in ms: " + Long.toString(descTime));
+        System.out.println("sorted random array, time in ms: " + Long.toString(randomTime));
+        System.out.println("sorted same values array, time in ms: " + Long.toString(sameTime));
+
+
+        System.out.println("\nQuickSort times using Lomuto (default) partitioning follow for this many records:" +
+        Integer.toString(NUM_ITEMS));
+        System.out.println("sorted asc array, time in ms: " + Long.toString(ascTime));
+        System.out.println("sorted desc array, time in ms: " + Long.toString(descTime));
+        System.out.println("sorted random array, time in ms: " + Long.toString(randomTime));
+        System.out.println("sorted same values array, time in ms: " + Long.toString(sameTime));
     }
 }
